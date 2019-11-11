6092858059b44d10fb106f07ce4cd234a4f00300
ejeino-7 <36841064+ejeino-7@users.noreply.github.com>
2019-11-11 10:48:49 +0100

    Update main.py

--- main.py
+++ main.py
@@ -4,10 +4,16 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def index():
-    return render_template('/home.html', header = "FROM PYFILE fran databas skit", body="RANDOM TEXT")
+    first = ["head", "desc", "https://i.ebayimg.com/images/g/ln4AAOSwkvFaXmcn/s-l400.jpg"]
+    i = 0
+    phones = []
+    while(i < 4):
+        phones.append(["head"+i, "desc"+i, "https://i.ebayimg.com/images/g/ln4AAOSwkvFaXmcn/s-l400.jpg"])
+        i++
+        
+    return render_template('/home.html', first = first, phones = phones)
 
 
 @app.route('/login')
