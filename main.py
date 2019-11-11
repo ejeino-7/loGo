8403a85071d4f11b31ae636ca72d3b606dcebb3b
ejeino-7 <36841064+ejeino-7@users.noreply.github.com>
2019-11-11 10:43:06 +0100

    Update main.py

--- main.py
+++ main.py
@@ -10,5 +10,13 @@ def index():
     return render_template('/home.html', header = "FROM PYFILE fran databas skit", body="RANDOM TEXT")
 
 
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
